15ee04e4626bfe54e0dff1fef3195e2272afb8e3
Mitch1802 <77403827+Mitch1802@users.noreply.github.com>
2024-03-12 20:51:05 +0100

    Create update.sh

---
+++ update.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+set -e
+echo "Katplan Update startet!"
+
+PROJECT_GIT_URL='https://Mitch1802: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@github.com/Mitch1802/Katplan_prod_2432.git'
+
+PROJECT_BASE_PATH='/app/Katplan_prod_2432'
+
+if [ -d $PROJECT_BASE_PATH ]
+then
+  rm -R $PROJECT_BASE_PATH
+fi
+
+mkdir -p $PROJECT_BASE_PATH
+git clone $PROJECT_GIT_URL $PROJECT_BASE_PATH
+
+cd $PROJECT_BASE_PATH
+docker compose up --build -d --remove-orphans
+
+rm -R $PROJECT_BASE_PATH
+
+echo "Katplan Update erfolgreich durchgeführt!"
