3fc6a5b9be2ca02c46ec28b6c40968e5e4e1cd0a
Mitch1802 <77403827+Mitch1802@users.noreply.github.com>
2024-03-14 11:53:26 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,8 +4,8 @@ set -e
 echo "Katplan Update startet!"
 
 PROJECT_GIT_URL='https://github.com/Mitch1802/Katplan_Test.git'
-
-PROJECT_BASE_PATH='/app/katplan_prod_2432'
+PROJECT_BASE_PATH='/app/katplan_2432'
+DOCKER_NETWORK_NAME='katplan_2432_nw'
 
 if [ -d $PROJECT_BASE_PATH ]
 then
@@ -16,6 +16,7 @@ mkdir -p $PROJECT_BASE_PATH
 git clone $PROJECT_GIT_URL $PROJECT_BASE_PATH
 
 cd $PROJECT_BASE_PATH
+docker network create -d bridge $DOCKER_NETWORK_NAME
 docker compose up --build -d --remove-orphans
 docker compose exec api python manage.py loaddata /app/backups/initial.json
 
